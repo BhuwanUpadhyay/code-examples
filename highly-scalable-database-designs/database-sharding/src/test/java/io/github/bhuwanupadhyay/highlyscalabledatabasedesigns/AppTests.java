ad4e76a84417071bba76476ab317e1760ee22cc2
Bhuwan Upadhyay <bhuwan.upadhyay@clusus.com>
2020-03-09 12:18:24 +0545

    chore: highly scalable database designs

---
+++ highly-scalable-database-designs/database-sharding/src/test/java/io/github/bhuwanupadhyay/highlyscalabledatabasedesigns/AppTests.java
@@ -0,0 +1,13 @@
+package io.github.bhuwanupadhyay.highlyscalabledatabasedesigns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
